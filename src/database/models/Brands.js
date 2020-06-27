6f825b6782561b9020b07263ec2aeb3a3c5435bf
Francisco <Francabj95@gmail.com>
2020-06-27 19:08:58 -0300

    cambio y verificacion

--- src/database/models/Brands.js
+++ src/database/models/Brands.js
@@ -1,6 +1,6 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const DB_brands = sequelize.define('brands', {
+  const Brands = sequelize.define('brands', {
     name: DataTypes.STRING,
     rating: DataTypes.TINYINT
   }, {
@@ -9,11 +9,11 @@ module.exports = (sequelize, DataTypes) => {
   DB_brands.associate = function(models) {
     console.log(DB_brands)
     // associations can be defined here
-    DB_brands.hasMany(models.DB_Articles,{  //put correctly the name of the DB (article)
+    Brands.hasMany(models.Article,{  //put correctly the name of the DB (article)
       as:'articles', //name table
       foreignKey:'brand_id' //name foreignKey, it's in the article table
     });
 
   };
-  return DB_brands;
+  return Brands;
 };
