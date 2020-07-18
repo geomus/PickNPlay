e2b4e014ddc4a3f03b0bccdab3cd2ef59ff12f5d
geomus <nahuel.daneri@outlook.com>
2020-07-18 18:28:29 -0300

    arreglo validacion en el front de products y users, inicio api

--- public/javascripts/validatorProducts.js
+++ public/javascripts/validatorProducts.js
@@ -1,6 +1,5 @@
 window.addEventListener("load", () => {
     let formProduct = document.querySelector("form.formProduct");
-    let errores = document.querySelector("form div.errors ul");
     let name = document.querySelector("input.name");
     let price = document.querySelector("input.price");
     let discount = document.querySelector("input.discount");
@@ -8,68 +7,129 @@ window.addEventListener("load", () => {
     let serialNumber = document.querySelector("input.serialNumber");
     let description = document.querySelector("textarea.description");
     let image = document.querySelector("input.image");
+    let inputs = document.querySelectorAll("input");
+
+    name.addEventListener("blur", ()=> {
+        if(name.value.trim() == "" || name.value.length < 5) {
+            name.classList.add("in-valid");
+            document.querySelector("small.errorName").style.display ='inline-block';
+        } else {
+            document.querySelector("small.errorName").style.display ='none';
+            name.classList.remove("in-valid");
+        }
+    })
+
+    price.addEventListener("blur", ()=> {
+        if (price.value.trim() == "" || Number(price.value) == 0) {
+            price.classList.add("in-valid");
+            document.querySelector("small.errorPrice").style.display ='inline-block';
+        } else {
+            document.querySelector("small.errorPrice").style.display ='none';
+            price.classList.remove("in-valid");
+        }
+    })
+
+    discount.addEventListener("blur", ()=> {
+        if (discount.value.trim() == "" || Number(discount.value) > 99) {
+            discount.classList.add("in-valid");
+            document.querySelector("small.errorDiscount").style.display ='inline-block';
+        } else {
+            document.querySelector("small.errorDiscount").style.display ='none';
+            discount.classList.remove("in-valid");
+        }
+    })
+
+    stock.addEventListener("blur", ()=> {
+        if (stock.value.trim() == "" || Number(stock.value) > 999 || Number(stock.value) == 0) {
+            stock.classList.add("in-valid");
+            document.querySelector("small.errorStock").style.display ='inline-block';
+        } else {
+            document.querySelector("small.errorStock").style.display ='none';
+            stock.classList.remove("in-valid");
+        }
+    })
+
+    serialNumber.addEventListener("blur", ()=> {
+        if (serialNumber.value.trim() == "") {
+            serialNumber.classList.add("in-valid");
+            document.querySelector("small.errorSerialNumber").style.display ='inline-block';
+        } else {
+            document.querySelector("small.errorSerialNumber").style.display ='none';
+            serialNumber.classList.remove("in-valid");
+        }
+    })
+
+    description.addEventListener("blur", ()=> {
+        if (description.value.trim() == "" || description.value.length < 20) {
+            description.classList.add("in-valid");
+            document.querySelector("small.errorDescription").style.display ='inline-block';
+        } else {
+            document.querySelector("small.errorDescription").style.display ='none';
+            description.classList.remove("in-valid");
+        }
+    })
+
+    //  validar formato de una imagen
+    function validateType (fileName){
+        let fileType = (fileName).split(".");
+        if((fileType.find(type => type=="jpg" || type=="jpeg" ||type=="gif" || type=="png")) !== undefined){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // validar formato de todas las imágenes (CORREGIR)
+    function validateTypeAll () {
+        let valid = [];
+        for (let i = 0; i < image.files.length; i++) {
+            if (validateType(image.files[i].name)) {
+                valid.push(true);
+            } else {
+                valid.push(false);
+            };
+        };
+        return valid;
+    }
+
+    image.addEventListener("blur", ()=> {
+        let errorImage = document.querySelector("small.errorImage");
+        if (validateTypeAll().includes(false)) {
+        // if (!validateType(image.value)) {
+            image.classList.add("in-valid");
+            errorImage.innerHTML = "Formatos de IMAGEN permitidos: JPG, JPEG, PNG, GIF, seleccionar de nuevo";
+            errorImage.style.display = 'inline-block';
+            image.value = "";
+        } else if(image.files.length > 3){
+            image.classList.add("in-valid");
+            errorImage.innerHTML = "Máximo 3 IMÁGENES por producto, seleccionar de nuevo";
+            errorImage.style.display = 'inline-block';
+            image.value = "";
+        }
+        else {
+            image.classList.remove("in-valid");
+            errorImage.style.display ='none';
+        };
+    })
 
 
     formProduct.addEventListener("submit", (e) => {
-        let errors = [];
-
-        if(name.value.trim() == "" || name.value.length < 5) {
-            errors.push("El NOMBRE es obligatorio, y de mínimo 5 caracteres");
-        }
-        if (price.value.trim() == "" || Number(price.value) == 0) {
-            errors.push("El PRECIO es obligatorio y no puede ser 0");
-        }
-        if (discount.value.trim() == "" || Number(discount.value) > 99) {
-            errors.push("El DESCUENTO no debe superar el 100%");
-        }
-        if (stock.value.trim() == "" || Number(stock.value) > 999 || Number(stock.value) == 0) {
-            errors.push("El STOCK debe estar entre 1 y 999");
-        }
-        if (serialNumber.value.trim() == "") {
-            errors.push("El Nº de SERIAL es obligatorio");
-        }
-        if (description.value.trim() == "" || description.value.length < 20) {
-            errors.push("La DESCRIPCIÓN debe tener un minimo de 20 caracteres");
-        }
-
-        //  validar formato de una imagen
-        function validateType (fileName){
-            let fileType = (fileName).split(".");
-            if((fileType.find(type => type=="jpg" || type=="jpeg" ||type=="gif" || type=="png")) !== undefined){
-                return true;
-            } else {
-                return false;
-            }
-        }
-
-        // validar formato de todas las imágenes (CORREGIR)
-        function validateTypeAll () {
-            let valid = [];
-            for (let i = 0; i < image.files.length; i++) {
-                if (validateType(image.files[i].name)) {
-                    valid.push(true);
+        // check errors, si hay errores no permite el submit
+        let hasErrors = () => {
+            let errores = [];
+            for (let i = 0; i < inputs.length; i++) {
+                if (inputs[i].classList.contains('in-valid')) {
+                   errores.push(true);
                 } else {
-                    valid.push(false);
+                    errores.push(false);
                 };
-            }
-            return valid;
-        }
-        // let validation = validateTypeAll();
+            };
+            return errores;
+        };
 
-        // if (validation.includes(false)) {
-        if (!validateType(image.value)) {
-            errors.push("Formatos de IMAGEN permitidos: JPG, JPEG, PNG, GIF, seleccionar de nuevo");
-            image.value = "";
-        } else if(image.files.length>3){
-            errors.push("Máximo 3 IMÁGENES por producto, seleccionar de nuevo");
-            image.value = "";
-        }
-
-        if (errors.length > 0) {
+        if (hasErrors().includes(true)) {
             e.preventDefault();
-            errors.forEach(error => {
-                errores.innerHTML += "<li>" + error + "</li>";
-            })
-        }
-    })
+            alert("Tiene errores");
+        };
+    });
 });
--- src/app.js
+++ src/app.js
@@ -35,12 +35,15 @@ app.set('views', path.join(__dirname, '/views'));
 const indexRouter = require('./routes/index');
 const productsRouter = require('./routes/products');
 const usersRouter = require('./routes/users');
+const usersApiRoute = require ('./routes/api/users');
 
 app.use(sessionUser);
 app.use('/', indexRouter); // Rutas /
 app.use('/products', productsRouter); // Rutas /products
 app.use('/users', usersRouter); // Rutas /users
 
+// API
+app.use('/api',usersApiRoute)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
