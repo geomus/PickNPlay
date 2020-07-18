d45e98ac19c08400d465fefe8d237f453816ff11
geomus <nahuel.daneri@outlook.com>
2020-07-18 18:28:07 -0300

    arreglo validacion en el front de products y users, inicio api

---
+++ public/javascripts/validatorUsers.js
@@ -0,0 +1,145 @@
+window.addEventListener("load", () => {
+    let formUser = document.querySelector('form.formUser');
+    let firstName = document.querySelector("input.firstName");
+    let lastName = document.querySelector('input.lastName');
+    let email = document.querySelector('input.email');
+    let pass = document.querySelector('input.pass');
+    let repass = document.querySelector('input.repass');
+    let avatar = document.querySelector("input.avatar");
+    let inputs = document.querySelectorAll("input");
+
+    let emailL = document.querySelector('input.emailL');
+    let passL = document.querySelector('input.passL');
+
+    // Validar caracteres
+    let expEm =/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    let exp =/^[A-Z]+$/i
+    let expP =/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,15}/;
+
+    firstName.addEventListener("blur", ()=> {
+        //  || !exp.test(firstName)
+        if (firstName.value.trim()=="" || firstName.value.length < 2 || !exp.test(firstName.value)) {
+            firstName.classList.add("in-valid");
+            document.querySelector("small.errorFirstName").style.display ='inline-block';
+        } else {
+            document.querySelector("small.errorFirstName").style.display ='none';
+            firstName.classList.remove("in-valid");
+        }
+    })
+
+    lastName.addEventListener("blur", ()=> {
+        if (lastName.value.trim()== "" || lastName.value.length < 2 || !exp.test(lastName.value)) {
+            lastName.classList.add("in-valid");
+            document.querySelector("small.errorLastName").style.display ='inline-block';
+        } else {
+            document.querySelector("small.errorLastName").style.display ='none';
+            lastName.classList.remove("in-valid");
+        }
+    })
+
+    email.addEventListener("blur", ()=> {
+        if (email.value.trim()== "" || email.value.length > 20 || !expEm.test(email.value)) {
+            email.classList.add("in-valid");
+            document.querySelector("small.errorEmail").style.display ='inline-block';
+        } else {
+            document.querySelector("small.errorEmail").style.display ='none';
+            email.classList.remove("in-valid");
+        }
+    })
+
+    pass.addEventListener("blur", ()=> {
+        //  || !expP.test(pass.value)
+        if (pass.value.trim()=="" || pass.value.length > 20 || pass.value.length <8) {
+            pass.classList.add("in-valid");
+            document.querySelector("small.errorPass").style.display ='inline-block';
+        } else {
+            document.querySelector("small.errorPass").style.display ='none';
+            pass.classList.remove("in-valid");
+        }
+    })
+
+    repass.addEventListener("blur", ()=> {
+        if (repass.value.trim()== "" || !(repass.value == pass.value)) {
+            repass.classList.add("in-valid");
+            document.querySelector("small.errorRepass").style.display ='inline-block';
+        } else {
+            document.querySelector("small.errorRepass").style.display ='none';
+            repass.classList.remove("in-valid");
+        }
+    })
+
+    //  validar formato de una imagen
+    function validateType (fileName){
+        let fileType = (fileName).split(".");
+        if((fileType.find(type => type=="jpg" || type=="jpeg" ||type=="gif" || type=="png")) !== undefined){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    avatar.addEventListener("blur", ()=> {
+        let errorAvatar = document.querySelector("small.errorAvatar");
+        if (!validateType(avatar.value)) {
+        // if (!validateType(image.value)) {
+            avatar.classList.add("in-valid");
+            errorAvatar.innerHTML = "Formatos de AVATAR permitidos: JPG, JPEG, PNG, GIF, seleccionar de nuevo";
+            errorAvatar.style.display = 'inline-block';
+            avatar.value = "";
+        } else if(avatar.files.length > 1){
+            avatar.classList.add("in-valid");
+            errorAvatar.innerHTML = "Máximo 1 AVATAR por usuario, seleccionar de nuevo";
+            errorAvatar.style.display = 'inline-block';
+            avatar.value = "";
+        }
+        else {
+            avatar.classList.remove("in-valid");
+            errorAvatar.style.display ='none';
+        };
+    })
+
+    // Login
+    emailL.addEventListener("blur", ()=> {
+        if (emailL.value.trim()== "" || emailL.value.length > 20) {
+            emailL.classList.add("in-valid");
+            document.querySelector("small.errorEmailL").style.display ='inline-block';
+        } else {
+            document.querySelector("small.errorEmailL").style.display ='none';
+            emailL.classList.remove("in-valid");
+        }
+    })
+
+    passL.addEventListener("blur", ()=> {
+        //  || !expP.test(passL.value) || passL.value.length <8
+        if (passL.value.trim()=="" || passL.value.length > 20) {
+            passL.classList.add("in-valid");
+            document.querySelector("small.errorPassL").style.display ='inline-block';
+        } else {
+            document.querySelector("small.errorPassL").style.display ='none';
+            passL.classList.remove("in-valid");
+        }
+    })
+
+    formUser.addEventListener("submit", (e) => {
+        // check errors, si hay errores no permite el submit
+        let hasErrors = () => {
+            let errores = [];
+            for (let i = 0; i < inputs.length; i++) {
+                if (inputs[i].classList.contains('in-valid')) {
+                   errores.push(true);
+                } else {
+                    errores.push(false);
+                };
+            };
+            return errores;
+        };
+
+        if (hasErrors().includes(true)) {
+            e.preventDefault();
+            alert("Tiene errores");
+        };
+    });
+});
+
+
+
---
+++ src/controllers/api/usersApiController.js
@@ -0,0 +1,187 @@
+let db = require("../../database/models");
+const fs = require("fs");
+const bcrypt = require("bcrypt");
+const path = require("path");
+const { validationResult } = require("express-validator");
+// const { Op } = db.Sequelize;
+
+const controller = {
+    list: async (req, res) =>{
+        const users = await db.Users.findAll();
+        res.json({
+            meta:{
+                status: 200,
+                count: users.length,
+                link: '/api/users/'
+            },
+            data: users.map(user => {
+                return {
+                    id: user.id,
+                    name: user.name,
+                    email: user.email,
+                    detail: `/api/profile/${user.id}`
+                }
+            })
+        })
+
+    },
+    // processLogin: (req, res, next) => {
+    //     let errors = validationResult(req);
+    //     if (!errors.isEmpty()) {
+    //         console.log(errors);
+    //         res.send(errors);
+    //     } else {
+    //         db.Users.findOne({
+    //             where: {
+    //                 email: req.body.email,
+    //             },
+    //         })
+    //             .then((userLog) => {
+    //                 if (userLog) {
+    //                     if (bcrypt.compareSync(req.body.pass, userLog.pass)) {
+    //                         //inicio de session
+    //                         // si se usa el delete pass para no mandar el password a la sesion la caga (debe generar un asincronismo),cuando queres logear por segunda vez el mismo user, no se bien por que?! por eso cree el objeto nuevo sin pass, me parece funcionaria tambien con un callback.
+    //                         let userSession = {
+    //                             id: userLog.id,
+    //                             firstName: userLog.firstName,
+    //                             lastName: userLog.lastName,
+    //                             email: userLog.email,
+    //                             avatar: userLog.avatar,
+    //                             isAdmin: userLog.isAdmin,
+    //                         };
+    //                         req.session.logedUser = userSession;
+    //                         //cookie
+    //                         if (req.body.remember) {
+    //                             //recordamos el usuario por 3 meses
+    //                             res.cookie("userLog", userLog, {
+    //                                 maxAge: 1000 * 60 * 60 * 24 * 90,
+    //                             });
+    //                             res.redirect(`/users/profile/${userLog.id}`);
+    //                         } else {
+    //                             //redirecciona a profile + id user
+    //                             res.redirect(`/users/profile/${userLog.id}`);
+    //                         }
+    //                     } else {
+    //                         res.send("Contraseña incorrecta");
+    //                     }
+    //                 } else {
+    //                     res.send("Usuario inexistente");
+    //                 }
+    //             })
+    //             .catch((error) => console.log(error));
+    //     }
+    // },
+    // logout: (req, res) => {
+    //     //eliminar cookie de recordar
+    //     res.clearCookie("userLog");
+    //     req.session.destroy((err) => {
+    //         res.redirect("/");
+    //     });
+    // },
+    // register: (req, res) => {
+    //     res.render("register", { title: "Registro" });
+    // },
+    // userAdd: async (req, res, next) => {
+    //     let errors = validationResult(req);
+    //     //console.log(errors);
+    //     //console.log(req.files[0]);
+
+    //     if (!errors.isEmpty()) {
+    //         if (req.files[0]) {
+    //             let imagenABorrar = path.join(
+    //                 __dirname,
+    //                 `../../public/images/userAvatars/${req.files[0].filename}`
+    //             );
+    //             fs.unlinkSync(imagenABorrar);
+    //         }
+
+    //         console.log(errors);
+    //         res.redirect('/');
+    //     } else {
+    //         delete req.body.repass;
+    //         req.body.pass = bcrypt.hashSync(req.body.pass, 10);
+    //         let newUser = {
+    //             ...req.body,
+    //             isAdmin: 0,
+    //             avatar: req.files[0].filename,
+    //         };
+    //         await db.Users.create(newUser);
+    //         //una vez logueado, iniciarle la sesion
+    //         let userLog = await db.Users.findOne({
+    //             where: {
+    //                 email: newUser.email,
+    //             },
+    //         });
+    //         let userSession = {
+    //             id: userLog.id,
+    //             firstName: userLog.firstName,
+    //             lastName: userLog.lastName,
+    //             email: userLog.email,
+    //             avatar: userLog.avatar,
+    //             isAdmin: userLog.isAdmin,
+    //         };
+    //         req.session.logedUser = userSession;
+    //         res.redirect("/");
+    //     }
+    // },
+    profile: async (req, res) => {
+        const user = await db.Users.findByPk(req.params.id)
+        res.json({
+            meta:{
+                status: 200,
+                link: `/api/profile/${user.id}`
+            },
+            data: {
+                    id: user.id,
+                    name: user.name,
+                    email: user.email,
+                    firstName: user.firstName,
+                    lastName: user.lastName,
+                    rating: user.rating,
+                    avatar: user.avatar
+                }
+            });
+    },
+    // edit: async (req, res) => {
+    //     let user = await db.Users.findByPk(req.params.id);
+    //     res.render("userEdit", {
+    //         title: "Editar usuario",
+    //         user,
+    //     }).catch((error) => console.log(error));
+    // },
+    // update: (req, res) => {
+    //     let errors = validationResult(req);
+    //     if (!errors.isEmpty()) {
+    //         console.log(errors);
+    //         res.redirect('/');
+    //     } else {
+    //         // preparar el body
+    //         let _body = {
+    //             ...req.body,
+    //             id: req.session.logedUser.id,
+    //             avatar: req.files[0].filename,
+    //             isAdmin: req.session.logedUser.isAdmin,
+    //         };
+    //         db.Users.update(_body, {
+    //             where: { id: req.params.id },
+    //         })
+    //             .then(() => res.redirect(`/users/profile/${req.params.id}`))
+    //             .catch((error) => console.log(error));
+    //     }
+    // },
+    // delete: async (req, res) => {
+    //     await db.Users.destroy({ where: { id: req.params.id } });
+    //     // borrar avatar
+    //     var avatarPath = path.join(
+    //         __dirname,
+    //         `../../public/images/userAvatars/${req.session.logedUser.avatar}`
+    //     );
+    //     await fs.unlinkSync(avatarPath);
+    //     // eliminar cookies y session
+    //     res.clearCookie("userLog");
+    //     req.session.destroy();
+    //     res.redirect("/");
+    // },
+};
+
+module.exports = controller;
